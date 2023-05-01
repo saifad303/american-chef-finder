8269fe6977ef3d15fbfd9f65f91fef0ce0cd10ae
Saif <saifad303@gmail.com>
2023-05-01 23:16:19 +0600

    initializing app and added all files.

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Root from "./pages/Root";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/Root.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Root = () => {
+  return <h1 className="text-3xl font-bold underline">This is root page.</h1>;
+};
+
+export default Root;
